79d6b07b955ea4693eb0e76da71c9c4be861b3fd
Jake <jakemacnaughton@gmail.com>
2017-12-04 23:20:15 -0500

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  resources :users, only: [:new, :create]
+  root 'users#welcome'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
