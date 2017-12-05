bc5d4bc4fc058f8d153d48df073c3badeebfafe9
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-12-04 23:04:13 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.password != @user.password_confirmation
+       redirect_to controller: 'users', action: 'new'
+    else
+      session[:user_id] = @user.id
+      redirect_to controller: 'welcome', action: 'home'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,12 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'welcome#home'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
 end
